53a6e14ad53bb0360fdaa04eaf621e5d38ad6702
Pierce <piercedavidraphael@gmail.com>
2021-12-15 11:08:24 -0600

    commit tree fa15a1a223a3b56f63a6335dbb7775ea2cef58f8

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,15 @@
 class Author < ApplicationRecord
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :phone_number, length: { is: 10 }
+
+    
+    # validate :unique_names
+
+    # def unique_names
+    #     if name == Author.find_by(name: name).name
+    #         errors.add(:name, "Unique names please.")
+    #     end
+    # end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+    ]
+
+    def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
+    # def fiction_or_nonfiction
+    #     if category.downcase == "Fiction" || "Non-Fiction"
+    #         errors.add(:category, "Fiction or Non-Fiction only!")
+    #     end
+    # end
 end
